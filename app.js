8acc410fd88e2f0172e634a0084e0a294bd2220e
Jesús Martínez <profjesus@users.noreply.github.com>
2019-04-08 18:37:13 +0200

    Files uploaded with the right path
    Now files are uploaded in the photos/{name} folder and use their original names.

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var multer = require('multer');
+
 var app = express();
 
 app.use(express.static('public'));
 
-var multer = require('multer')({
-    dest: 'public/photos'
-});
+var storage = multer.diskStorage(
+  {
+      destination: './public/photos/',
+      filename: function ( req, file, cb ) {
+        var dir = path.join('./public/photos', req.body.name);
+        if (!fs.existsSync(dir)){
+          fs.mkdirSync(dir);
+        }
+        cb(null, path.join(req.body.name, file.originalname));
+      }
+  }
+);
+
+var upload = multer( { storage: storage } );
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.post('/upload', [multer.fields([{ name: 'original' }, { name: 'edited' }])], function (req, res, next) {
-    res.send('Ok!');
+app.post('/upload', [upload.fields([{ name: 'original' }, { name: 'edited' }])], function (req, res, next) {
+  res.send('Ok!');
 });
 
 app.listen(8080, function () {
