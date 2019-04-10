1b5ed0137520b4ca85dc381302adab9e6d23fa79
Jesús Martínez <profjesus@users.noreply.github.com>
2019-04-10 13:38:37 +0200

    Timer added
    A timer counts the seconds it took to choose a photo

--- public/script.js
+++ public/script.js
@@ -1,11 +1,14 @@
 getUsers();
 
+var timerDisplay = document.querySelector('#crono');
+var startTime, tInterval, seconds;
+
 $('#user').change(function() {
     getImagesForUser($(this).val());
 });
 
 function getUsers() {
-    $.get("http://localhost:8080/users/", function (data) {
+    $.get("/users/", function (data) {
         if (data) {
             for (var i=0; i<data.length; i++) {
                 $("#user").append('<option value="' + data[i] + '">' + data[i] + '</option>');
@@ -17,15 +20,32 @@ function getUsers() {
 }
 
 function getImagesForUser(user) {
-    $("#imageleft").append('<button type="button" data-toggle="modal" data-target="#resultModal" class="btn" id="original"><img src="/photos/' + user + '/original" width="100%"/></button>');
-    $("#imageright").append('<button type="button" data-toggle="modal" data-target="#resultModal" class="btn" id="edited"><img src="/photos/' + user + '/edited" width="100%"/></button>');
+    $("#imageleft").html('<button type="button" data-toggle="modal" data-target="#resultModal" class="btn" id="original"><img src="/photos/' + user + '/original" width="100%"/></button>');
+    $("#imageright").html('<button type="button" data-toggle="modal" data-target="#resultModal" class="btn" id="edited"><img src="/photos/' + user + '/edited" width="100%"/></button>');
     $('#original').click(function() {
-        $('#resultModalLabel').append("Oooops!");
-        $('#resultModalBody').append("You chose poorly");
+        $('#resultModalLabel').html("Oooops!");
+        $('#resultModalBody').html("You chose poorly");
+        clearInterval(tInterval); // Stop timer
     });
     $('#edited').click(function() {
-        $('#resultModalLabel').append("Great!");
-        $('#resultModalBody').append("That was indeed the edited image");
+        $('#resultModalLabel').html("Great!");
+        $('#resultModalBody').html("That was indeed the edited image. It took you <strong>" + seconds + "</strong> seconds to find out");
+        clearInterval(tInterval); // Stop timer
     });
     $("#images").show();
+    startTime = new Date().getTime();
+    printTime();
+    tInterval = setInterval(printTime, 1000);
 }
+
+// Showing timer
+function printTime(){
+    var updatedTime = new Date().getTime();
+    var difference =  updatedTime - startTime;
+    seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    timerDisplay.innerHTML = seconds;
+}
+
+$('#resultModalClose').click(function() {
+    $("#images").hide();
+});
