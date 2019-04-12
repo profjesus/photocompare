c415f2b6750c07ab97f9e816ddfa3492f7878b7b
Jesús Martínez <suso@MacBook-Pro-de-Jesus-2.local>
2019-04-12 02:10:28 +0200

    Original and edited photos shown on the left or right randomly

--- public/script.js
+++ public/script.js
@@ -2,9 +2,12 @@ getUsers();
 
 var timerDisplay = document.querySelector('#crono');
 var startTime, tInterval, seconds;
+var first = true;
+var currentUser;
 
 $('#user').change(function() {
-    getImagesForUser($(this).val());
+    currentUser = $(this).val();
+    getImagesForUser(currentUser);
 });
 
 function getUsers() {
@@ -20,8 +23,21 @@ function getUsers() {
 }
 
 function getImagesForUser(user) {
-    $("#imageleft").html('<button type="button" data-toggle="modal" data-target="#resultModal" class="btn" id="original"><img src="/photos/' + user + '/original" width="100%"/></button>');
-    $("#imageright").html('<button type="button" data-toggle="modal" data-target="#resultModal" class="btn" id="edited"><img src="/photos/' + user + '/edited" width="100%"/></button>');
+    var htmlOriginalImg = '<button type="button" data-toggle="modal" data-target="#resultModal" class="btn" id="original"><img src="/photos/'
+    htmlOriginalImg += user + '/original';
+    htmlOriginalImg += first ? '1' : '2';
+    htmlOriginalImg += '" width="100%"/></button>';
+    var htmlEditedImg = '<button type="button" data-toggle="modal" data-target="#resultModal" class="btn" id="edited"><img src="/photos/'
+    htmlEditedImg += user + '/edited';
+    htmlEditedImg += first ? '1' : '2';
+    htmlEditedImg += '" width="100%"/></button>';
+    if (Math.random() >= 0.5) {
+        $("#imageleft").html(htmlOriginalImg);
+        $("#imageright").html(htmlEditedImg);
+    } else {
+        $("#imageleft").html(htmlEditedImg);
+        $("#imageright").html(htmlOriginalImg);
+    }
     $('#original').click(function() {
         $('#resultModalLabel').html("Oooops!");
         $('#resultModalBody').html("You chose poorly");
@@ -47,5 +63,10 @@ function printTime(){
 }
 
 $('#resultModalClose').click(function() {
-    $("#images").hide();
+    first = !first;
+    if (!first) {
+        getImagesForUser(currentUser);
+    } else {
+        $("#images").hide();
+    }    
 });
